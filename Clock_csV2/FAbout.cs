3153cb83998914dcaa5e15beb7cced11b67286b9
Leon68 <leon-dev@ukr.net>
2022-08-09 00:39:44 +0300

    add ver win

--- Clock_csV2/FAbout.cs
+++ Clock_csV2/FAbout.cs
@@ -71,9 +71,9 @@ namespace frmAbout
             // 
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.button1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.button1.Location = new System.Drawing.Point(353, 70);
+            this.button1.Location = new System.Drawing.Point(353, 77);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(73, 36);
+            this.button1.Size = new System.Drawing.Size(73, 38);
             this.button1.TabIndex = 0;
             this.button1.Text = "&Ok";
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -93,9 +93,9 @@ namespace frmAbout
             // groupBox1
             // 
             this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.groupBox1.Location = new System.Drawing.Point(6, 51);
+            this.groupBox1.Location = new System.Drawing.Point(6, 50);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(420, 3);
+            this.groupBox1.Size = new System.Drawing.Size(420, 13);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             // 
@@ -104,7 +104,7 @@ namespace frmAbout
             this.label2.AutoSize = true;
             this.label2.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label2.Location = new System.Drawing.Point(11, 62);
+            this.label2.Location = new System.Drawing.Point(11, 69);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(17, 20);
             this.label2.TabIndex = 4;
@@ -115,7 +115,7 @@ namespace frmAbout
             this.label3.AutoSize = true;
             this.label3.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label3.Location = new System.Drawing.Point(11, 86);
+            this.label3.Location = new System.Drawing.Point(11, 93);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(17, 20);
             this.label3.TabIndex = 5;
@@ -136,7 +136,7 @@ namespace frmAbout
             // 
             this.AcceptButton = this.button1;
             this.AutoScaleBaseSize = new System.Drawing.Size(7, 20);
-            this.ClientSize = new System.Drawing.Size(436, 113);
+            this.ClientSize = new System.Drawing.Size(436, 122);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.groupBox1);
@@ -175,7 +175,7 @@ namespace frmAbout
 			}
 
 
-            label1.Text = "Clock Version " + Application.ProductVersion; 
+            label1.Text = "Clock Version " + Application.ProductVersion;
 
             //ManagementObjectSearcher s = new ManagementObjectSearcher("SELECT Caption, Version, ServicePackMajorVersion, TotalVisibleMemorySize FROM Win32_OperatingSystem");
 
@@ -191,10 +191,96 @@ namespace frmAbout
             //    winmem = "Physical memory available: " + valmem.ToString("### ### ### ###") + " KB";
 
             //}
-            
+
             //label2.Text = winver;
             //label3.Text = winmem;
-		}
 
-	}
+            label2.Text = getOSInfo();
+        }
+
+        private string getOSInfo()
+        {
+            //Get Operating system information.
+            OperatingSystem os = Environment.OSVersion;
+            //Get version information about the os.
+            Version vs = os.Version;
+
+            //Variable to hold our return value
+            string operatingSystem = "";
+
+            if (os.Platform == PlatformID.Win32Windows)
+            {
+                //This is a pre-NT version of Windows
+                switch (vs.Minor)
+                {
+                    case 0:
+                        operatingSystem = "95";
+                        break;
+                    case 10:
+                        if (vs.Revision.ToString() == "2222A")
+                            operatingSystem = "98SE";
+                        else
+                            operatingSystem = "98";
+                        break;
+                    case 90:
+                        operatingSystem = "Me";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (os.Platform == PlatformID.Win32NT)
+            {
+                switch (vs.Major)
+                {
+                    case 3:
+                        operatingSystem = "NT 3.51";
+                        break;
+                    case 4:
+                        operatingSystem = "NT 4.0";
+                        break;
+                    case 5:
+                        if (vs.Minor == 0)
+                            operatingSystem = "2000";
+                        else
+                            operatingSystem = "XP";
+                        break;
+                    case 6:
+                        if (vs.Minor == 0)
+                            operatingSystem = "Vista";
+                        else if (vs.Minor == 1)
+                            operatingSystem = "7";
+                        else if (vs.Minor == 2)
+                            operatingSystem = "8";
+                        else
+                            operatingSystem = "8.1";
+                        break;
+                    case 10:
+                        operatingSystem = "10";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            //Make sure we actually got something in our OS check
+            //We don't want to just return " Service Pack 2" or " 32-bit"
+            //That information is useless without the OS version.
+            if (operatingSystem != "")
+            {
+                //Got something.  Let's prepend "Windows" and get more info.
+                operatingSystem = "Windows " + operatingSystem;
+                //See if there's a service pack installed.
+                if (os.ServicePack != "")
+                {
+                    //Append it to the OS name.  i.e. "Windows XP Service Pack 3"
+                    operatingSystem += " " + os.ServicePack;
+                }
+                //Append the OS architecture.  i.e. "Windows XP Service Pack 3 32-bit"
+                //operatingSystem += " " + getOSArchitecture().ToString() + "-bit";
+            }
+            //Return the information we've gathered.
+            return operatingSystem;
+        }
+
+    }
 }
